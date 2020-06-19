8bd8180e3ad50a470586d65eded9fc50c46b3d90
Santiago Gonzalez Fernandez <santiagogonzalezfernandezpro@gmail.com>
2020-06-19 13:56:11 -0300

    Empiezo a hacer la app de componentes (seccion 6)

---
+++ componentes/lib/main.dart
@@ -0,0 +1,18 @@
+//Importaciones que no son mias
+import 'package:flutter/material.dart';
+
+//Importaciones que si son mias
+import 'package:componentes/src/pages/home_temp.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Componentes APP',
+      home: HomePageTemp()
+    );
+  }
+}
